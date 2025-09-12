6f83d6f4760e7218216858706660cdcff06edb38
Vishal Tambi <tambivishal3@gmail.com>
2025-09-12 18:57:10 +0530

    fix api handler

--- server/server.js
+++ server/server.js
@@ -59,8 +59,8 @@ app.use((err, req, res, next) => {
 });
 
 // Catch all handler for API
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 
